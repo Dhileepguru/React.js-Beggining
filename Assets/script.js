58d4dc342bad609a0b9890596ace2c5a8a44eb90
Dhileepguru <dhileepguru497@gmail.com>
2024-08-03 09:47:07 +0530

    Inserted Nested element

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,35 @@
-const name=React.createElement("h1",{id: "apple",placeholder:"guru"},"Hello World");
+
+const parent=React.createElement("div",{id:"parent"},
+        [
+                React.createElement("div",{id:"child"},
+                [
+                React.createElement("h1",{},"Hi I am h1 tag"),
+                React.createElement("h2",{},"Hi I am h2 tag"),
+                ]),
+
+                React.createElement("div",{id:"child2"},
+                        React.createElement("h1",{},"Hi I am h1 tag"))
+        ]
+        );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const name=React.createElement("h1",{id: "apple",placeholder:"guru"},"Hello World");
         const root=ReactDOM.createRoot(document.getElementById("root"));
-        root.render(name);
+        root.render(parent);
 
