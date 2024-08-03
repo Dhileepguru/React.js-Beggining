d77de0df8ed4028f6158a268c5459bbf216f0118
Dhileepguru <dhileepguru497@gmail.com>
2024-08-03 09:13:45 +0530

    Updated Hello world

---
+++ Assets/script.js
@@ -0,0 +1,4 @@
+const name=React.createElement("h1",{id: "apple",placeholder:"guru"},"Hello World");
+        const root=ReactDOM.createRoot(document.getElementById("root"));
+        root.render(name);
+
